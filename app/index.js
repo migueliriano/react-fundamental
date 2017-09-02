dd013514019c160db777c601989e2f00d76bcd6d
migueliriano <ing.miguel.php@hotmail.com>
2017-09-01 23:28:17 -0400

    Add popular component

--- app/index.js
+++ app/index.js
@@ -1,20 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+var App = require('./components/App');
 
 //state
 // lifecycle event
 // UI
-
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World, I'm I Rock start
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(
     <App />,
